822aa0a279cdfcf723d30f421b744708c2d51527
eduardosanandres <eliu2424@hotmail.com>
2019-02-01 17:35:45 -0500

    mas cambios

--- app/AltaDireccion.php
+++ app/AltaDireccion.php
@@ -9,4 +9,10 @@ class AltaDireccion extends Model
     protected $table = "alta_direccion";
     protected $primaryKey = "id_facultad";
     protected $fillable = ["id_facultad","nombre"];
+
+     public function contrato(){
+
+        return $this->hasMany("App\AltaDireccion","id_facultad","id_facultad");
+
+    }
 }
