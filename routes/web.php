935828203a19e1054fb2734428ade98ebb68608c
eduardosanandres <eliu2424@hotmail.com>
2019-02-13 12:54:33 -0500

    cambios

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,7 @@
 |
 */
 
-/*Route::get('/', function () {
+Route::get('/', function () {
     return view('welcome');
 });
 Route::resource('/EstudiosBasicos', 'EstudiosBasicosController');
@@ -24,29 +24,8 @@ Route::get('/insertarestudiosbasicos', 'EstudiosBasicosController@store');
 Route::get('/actualizarestudiosbasicos', 'EstudiosBasicosController@update');
 Route::get('/borrarestudiosbasicos', 'EstudiosBasicosController@destroy');
 
-*/
 
 
 
 
-Auth::routes();*/
 
-Route::get('/home', 'HomeController@index')->name('home');
-
-<<<<<<< HEAD
-=======
-//Rutas para Datos Personales:
-//Route::get('/datos/listar', 'DatosPersonalesController@index');
-/*Route::get('/datos', 'DatosPersonalesController@index');
-Route::get('/crear', 'DatosPersonalesController@create');
-Route::get('/actualizar', 'DatosPersonalesController@update');
-Route::get('/insertar', 'DatosPersonalesController@insert');
-Route::get('/eliminar', 'DatosPersonalesController@delete');*/
-
-Route::resource('/datos','DatosController');
-Route::get('/listarIdiomas', 'DatosController@listarIdiomas')->name('datos.listarIdiomas');
-Route::get('/listarHabientes', 'DatosController@listarHabientes')->name('datos.listarHabientes');
-Route::get('/listarTipoIdioma', 'DatosController@listarTipoIdiomas')->name('datos.listarTipoIdiomas');
-//Route::get('/datos/listar','DatosController@listarIdiomas')->name('datos.listarIdiomas');
-//Route::resource('/idioma','IdiomaController');
->>>>>>> 111fa53aa521bb39f92b2f7f4026c3e6d16b69f7
