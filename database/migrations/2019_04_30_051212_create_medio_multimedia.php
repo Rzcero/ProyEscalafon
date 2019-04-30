3d4de8827a756f1b3fb9705a517863a6bf686a42
Rzcero <unprgunprg@gmail.com>
2019-04-30 01:21:57 -0500

    chevez

--- database/migrations/2019_04_30_051212_create_medio_multimedia.php
+++ database/migrations/2019_04_30_051212_create_medio_multimedia.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateMedioEscrito extends Migration
+class CreateMedioMultimedia extends Migration
 {
     /**
      * Run the migrations.
